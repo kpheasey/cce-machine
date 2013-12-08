db3bbeb35aea805b05fa1fbcc2a354e4ee48880e
Kevin <kevin.pheasey@gmail.com>
2013-12-08 12:32:04 -0500

    Don't link database.yml for cap deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,6 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
 
-set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :default_env, { path: "/opt/ruby/bin:$PATH" }
