cf7f0d8a6ede0ddf7b1d1f61148eb39d43a1fba2
Kevin <kevin.pheasey@gmail.com>
2013-12-08 12:24:05 -0500

    Capistrano setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,18 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'btc'
+set :repo_url, 'git@github.com:kpheasey/btc.git'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :deploy_to, '/var/www/apps/www.btc'
+set :scm, :git
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
 
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
 
 namespace :deploy do
 
