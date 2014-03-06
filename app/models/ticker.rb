a8504a47f3fd55431903a38d93c822da662d6ecc
Kevin <kevin.pheasey@gmail.com>
2014-03-06 17:48:54 -0500

    Add hour, day, week, and month ticker increments.

--- app/models/ticker.rb
+++ app/models/ticker.rb
@@ -5,17 +5,20 @@ class Ticker < ActiveRecord::Base
   def self.process(trades)
     exchange_id = trades.first.exchange_id
     market_id = trades.first.market_id
-    minutes, hours, days, months = get_increments(trades)
+    minutes, hours, days, weeks, months = get_increments(trades)
 
-    minutes.each do |minute|
-      Ticker::OneMinute.process(minute, exchange_id, market_id)
-    end
+    minutes.each{ |minute| Ticker::OneMinute.process(minute, exchange_id, market_id) }
+    hours.each{ |hour| Ticker::OneHour.process(hour, exchange_id, market_id) }
+    days.each{ |day| Ticker::OneDay.process(day, exchange_id, market_id) }
+    weeks.each{ |week| Ticker::OneWeek.process(week, exchange_id, market_id) }
+    months.each{ |month| Ticker::OneMonth.process(month, exchange_id, market_id) }
   end
 
   def self.get_increments(trades)
     minutes = []
     hours = []
     days = []
+    weeks = []
     months = []
 
     trades.each do |trade|
@@ -28,10 +31,13 @@ class Ticker < ActiveRecord::Base
       day = trade.date.beginning_of_day
       days << day unless days.include? day
 
-      month = trade.date.month
+      week = trade.date.beginning_of_week
+      weeks << week unless weeks.include? week
+
+      month = trade.date.beginning_of_month
       months << month unless months.include? month
     end
 
-    return minutes, hours, days, months
+    return minutes, hours, days, weeks, months
   end
 end
--- app/models/ticker/one_hour.rb
+++ app/models/ticker/one_hour.rb
@@ -1,3 +1,34 @@
 class Ticker::OneHour < Ticker
 
+  def self.process(start_time, exchange_id, market_id)
+    trades = trades(start_time, exchange_id, market_id)
+
+    return if trades.empty?
+
+    tick = Ticker::OneHour.find_or_initialize_by(
+        exchange_id: exchange_id,
+        market_id: market_id,
+        timestamp: start_time.beginning_of_hour
+    )
+
+    tick.exchange_id = exchange_id
+    tick.market_id = market_id
+    tick.high = trades.max_by(&:price).price
+    tick.open = trades[-1].price
+    tick.close = trades[0].price
+    tick.low = trades.min_by(&:price).price
+    tick.volume = trades.map(&:amount).inject(0, &:+)
+    tick.count = trades.size
+    tick.save
+  end
+
+  def self.trades(start_time, exchange_id, market_id)
+    end_time = start_time.end_of_hour
+
+    trades = Trade.where(exchange_id: exchange_id, market_id: market_id).
+        where('date >= ? AND date <= ?', start_time, end_time)
+
+    return trades
+  end
+
 end
