466058cd516ad387bfd26e74311b512ae1b7917a
Kevin <kevin.pheasey@gmail.com>
2014-03-07 10:40:19 -0500

    Fix sidekiq deployment tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,23 @@ end
 
 after 'deploy:publishing', 'deploy:restart'
 
+# upstart
+namespace :upstart do
+  desc 'Generate and upload Upstard configs for daemons needed by the app'
+  task :update_configs, except: {no_release: true} do
+    upstart_config_files = File.expand_path('../upstart/*.conf.erb', __FILE__)
+    upstart_root         = '/etc/init'
+
+    Dir[upstart_config_files].each do |upstart_config_file|
+      config = ERB.new(IO.read(upstart_config_file)).result(binding)
+      path   = "#{upstart_root}/#{File.basename upstart_config_file, '.erb'}"
+
+      put config, path
+    end
+  end
+end
+
+after 'deploy:update_code', 'upstart:update_configs'
 
 # sidekiq
 namespace :sidekiq do
@@ -63,7 +80,7 @@ namespace :sidekiq do
   end
 end
 
-before 'deploy:update_code', 'sidekiq:quiet'
-after  'deploy:stop',        'sidekiq:stop'
-after  'deploy:start',       'sidekiq:start'
-before 'deploy:restart',     'sidekiq:restart'
+after  'deploy:starting', 'sidekiq:quiet'
+after 'deploy:updated',   'sidekiq:stop'
+after 'deploy:published', 'sidekiq:start'
+before 'deploy:restart',  'sidekiq:restart'
